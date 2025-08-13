0e80bd643b53a9e3e716f9f041011f671d1ee423
jonneldev <jonneldosado.code@gmail.com>
2025-08-14 00:10:25 +0800

    install axios

--- exercises/part2/countries/src/App.jsx
+++ exercises/part2/countries/src/App.jsx
@@ -1,6 +1,49 @@
+import { useState, useEffect } from "react"
+import axios from 'axios'
+
 function App() {
+  const url = 'https://studies.cs.helsinki.fi/restcountries/'
+  const [countries, setCountries] = useState(null)
+  const [value, setValue] = useState('')
+
+  useEffect(() => {
+    console.log(`effect run`);
+
+    if(countries) {
+      console.log(`fetching countries`)
+      axios
+        .get(`${url}api/all/`)
+        .then(response => {
+          console.log(response)
+        })
+    } else {
+      console.log(`empty countries`)
+    }
+    
+  }, [countries])
+
+  const handleChange = (event) => {
+    setValue(event.target.value)
+  }
+
+  
   return (
-    <h1>countries</h1>
+    <div>
+      <div>
+        find countries
+        <input 
+          style={{
+            display: "inline-block",
+            marginLeft: "5px",
+          }}
+          value={value}
+          onChange={handleChange}
+        />
+      </div>
+      <div>
+
+      </div>
+    </div>
   )
 }
 
