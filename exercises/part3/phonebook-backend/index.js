20e6b9bdf9c4c1da0af32690dee212a800924440
jonneldev <jonneldosado.code@gmail.com>
2025-09-12 23:21:01 +0800

    test2

--- exercises/part3/phonebook-backend/index.js
+++ exercises/part3/phonebook-backend/index.js
@@ -34,6 +34,10 @@ let persons = [
     }
 ]
 
+app.get("/", (req, res) => {
+  res.send("<h1>Phonebook Backend is running!</h1>")
+})
+
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
