b51b5a917a443e1b8d84ba07a32de114bfa26577
jonneldev <jonneldosado.code@gmail.com>
2025-09-18 02:11:43 +0800

    database

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -33,6 +33,10 @@ app.use(express.static('dist'))
 app.use(requestLogger)
 app.use(express.json())
 
+app.get("/", (req, res) => {
+  res.send("<h1>Notes Backend is running!</h1>")
+})
+
 app.get('/api/notes', (request, response) => {
   response.json(notes)
 })
@@ -87,7 +91,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3000
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Notes server running on port ${PORT}`)
 })
